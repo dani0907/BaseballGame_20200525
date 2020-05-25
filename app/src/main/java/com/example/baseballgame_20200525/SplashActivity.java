8960c87c9da968498402f771bdf9345614e661a2
dy696786 <dy696786@gmail.com>
2020-05-25 13:25:33 +0900

    스플래쉬화면 추가 + 이 화면을 시작점으로 변경

---
+++ app/src/main/java/com/example/baseballgame_20200525/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.baseballgame_20200525;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
